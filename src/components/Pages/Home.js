d61fe33c194dd9c1a3dab2c13fe50e6dc29a4f0a
axel <axel-bentancur@hotmail.com>
2020-07-18 21:17:45 -0300

    project finished

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,3 +3,21 @@ import React from "react";
 export default function Home() {
   return <div>Home</div>;
 }
+
+
+//--- code for home to recognize if you are logged and if dont automatically send to login page ---//
+
+/* import React, {useEffect, useContext} from "react";
+import { useHistory } from "react-router-dom";
+import UserContext from '../../context/UserContext'
+
+export default function Home() {
+  const {userData} = useContext(UserContext);
+  const history = useHistory();
+
+  useEffect(()=>{
+    if(!userData.user) history.push('/login');
+  });
+
+  return <div>Home</div>;
+} */
--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,5 +1,58 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import UserContext from "../../context/UserContext";
+import Axios from "axios";
+import ErrorNotice from "../Misc/ErrorNotice";
 
 export default function Login() {
-  return <div>Login</div>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState();
+
+  const { setUserData } = useContext(UserContext);
+  const history = useHistory();
+
+  const submit = async (e) => {
+    e.preventDefault();
+    try{
+    const loginUSer = { email, password };
+    const loginRes = await Axios.post(
+      "http://localhost:5000/users/login",
+      loginUSer
+    );
+    setUserData({
+      token: loginRes.data.token,
+      user: loginRes.data.user,
+    });
+    localStorage.setItem("auth-token", loginRes.data.token);
+    history.push("/");
+  }catch(err){
+    err.response.data.msg && setError(err.response.data.msg);
+  }
+  };
+
+  return (
+    <div className="page">
+      <h2>LogIn</h2>
+      {error && (
+        <ErrorNotice message={error} clearError={() => setError(undefined)} />
+      )}
+      <form onSubmit={submit}>
+        <label>Email</label>
+        <input
+          id="login-email"
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label>password</label>
+        <input
+          id="login-password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+  );
 }
